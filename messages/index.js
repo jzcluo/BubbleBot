5494b9ac06e4eca82624ccfe869839d2a820b594
JLuo <jeffreyzcluo@gmail.com>
2017-09-25 00:13:58 -0400

    Solving multiple entry points issue

--- messages/index.js
+++ messages/index.js
@@ -3,6 +3,7 @@ const builder = require("botbuilder");
 const botbuilder_azure = require("botbuilder-azure");
 const path = require('path');
 const request = require('superagent');
+const req = require('request');
 require('dotenv').config();
 
 
@@ -34,7 +35,43 @@ const bot = new builder.UniversalBot(connector, [
 //whether to persist conversationdata
 //bot.set(`persistConversationData`, false);
 
-bot.dialog('qnadialog', require('./qnadialog.js')).triggerAction({
+bot.dialog('qnadialog', (session, args, next) => {
+    session.sendTyping();
+    console.log('qnamaker called');
+    session.sendTyping();
+    const questionAsked = session.message.text;
+    const bodyText = JSON.stringify({question : questionAsked});
+    const host = `https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/`;
+    const url = `${host}knowledgebases/${process.env.KnowledgeBaseID}/generateAnswer`;
+    console.log(url);
+
+    const options = {
+        url : url,
+        method : 'POST',
+        body : bodyText,
+        headers : {
+            'Ocp-Apim-Subscription-Key' : process.env.QnASubscriptionKey
+        }
+    };
+
+    req(options , (err, res, body) => {
+        if (err) {
+            console.log(err);
+            session.endConversation('Sorry something went wrong');
+        } else {
+            const response = JSON.parse(body)['answers'][0];
+            console.log(response);
+            if (response.score > 60) {
+                session.endConversation(response.answer);
+            } else if (response.score > 30) {
+                session.send('I am not sure if this is right');
+                session.endConversation(response.answer);
+            } else {
+                session.endConversation('sorry I do not have the answer you need');
+            }
+        }
+    });
+}).triggerAction({
     matches : 'QnAIntent'
 });
 
